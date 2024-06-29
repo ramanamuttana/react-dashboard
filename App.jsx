1bc098d58813039d91e2c7e4fe533fe0286a1ee5
ramanamuttana <ramanamuttana@gmail.com>
2024-06-29 18:09:38 +0200

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BonnWeatherDashboard from './BonnWeatherDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <BonnWeatherDashboard />
+    </div>
+  );
+}
+
+export default App;
